2d6ef8efc7824a30384df25509b2e2ec2068756a
jimmulus <jimmulus@gmail.com>
2017-08-02 21:04:37 +0200

    Added form for submit message and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,24 @@
 var socket = io();
 
-socket.on('connect', function () {
-    console.log('Connected to server');
-
-    socket.emit('createMessage', {        
-        from: 'User 1', 
-        text: 'Hello to everyone! '
-    });
-});
-
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
     console.log('New message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });
